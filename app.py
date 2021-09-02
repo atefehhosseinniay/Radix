943322072b64e5d228488db794e44dc63850ec57
Camille De Neef <camille.deneef@gmail.com>
2021-09-02 09:18:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ from utils.preprocessing.read_pdf import PDFExtractor
 
 if __name__ == '__main__':
   pdfs = PDFExtractor(dir = './data/pdf/', max_iter = None)
-  i = 0
+  
   # Iter trough every pdf of the directory
   for pdf in pdfs:
-    i += 1
     # pdf contains the extracted text from the pdf file
-    # print(pdf)
+    print(pdf)
     # Preprocess every pdf here (preporcessing not done yet)
-    # print('-'*50)
-  print(i)
+    print('-'*50)
+ 
 
   # Get pdf with file name, here gets ./data/pdf/3000.pdf
   print(len(pdfs.get_pdf_by_name('3000.pdf')))
