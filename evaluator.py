e06602b56ee269d17c8c09c6666f3c5e83911bad
VincentKervyn <83582004+VincentKervyn@users.noreply.github.com>
2021-09-09 16:01:35 +0200

    Update evaluator.py
    Add the cosine evaluation, because it's based on vectorization too

--- evaluator.py
+++ evaluator.py
@@ -2,6 +2,7 @@ from pyresparser import ResumeParser
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 directory = '/home/vincent/PycharmProjects/pyreparser/Radix/data/pdf/'
 '''
@@ -14,7 +15,7 @@ file_3 = '3.pdf'
 file_4 = '4.pdf'
 file_5 = '5.pdf'
 
-#pyreparser creat dictionaries, I need string here for test
+#pyreparser create dictionaries, I need string here for test
 # but it's not smart because we need this dictionaries to get the sections as 'skills'
 data_1 = str(ResumeParser(directory+file_1).get_extracted_data())
 data_2 = str(ResumeParser(directory+file_2).get_extracted_data())
@@ -27,10 +28,17 @@ corpus = [data_1, data_2, data_3, data_4, data_5]
 '''
 Let's do the magic of vectorisation here 
 '''
+# cosine similarity
+#it work with 2 vector at the time
 cv = CountVectorizer(strip_accents= 'unicode', lowercase= True, stop_words='english')
 count_matrix = cv.fit_transform(corpus)
+# get the match percentage
+matchPercentage = cosine_similarity(count_matrix)[0][1] * 100
+matchPercentage = round(matchPercentage, 2) # round to two decimal
+print( 'sklearn cosine similarity: '+str(file_4)+" matches about "+ str(matchPercentage)+ " % of the "+ str(file_3))
 
-vect = TfidfVectorizer(min_df=1, stop_words="english") #yeah stop words again
+#TF-IDF similarity
+vect = TfidfVectorizer(min_df=1, stop_words="english")
 tfidf = vect.fit_transform(corpus)
 pairwise_similarity2 = tfidf * tfidf.T #product to create a square matrix
 print ("pairwise simi 2 is ")
